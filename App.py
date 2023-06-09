c708a36aea6c951f0259d75da66417f256acde7f
ElCresp0 <jacky8203@protonmail.com>
2023-06-09 04:32:54 +0200

    working version

---
+++ App.py
@@ -0,0 +1,85 @@
+# https://app.swaggerhub.com/apis/NobelMedia/NobelMasterData/2.1
+import datetime
+import requests
+import re
+from constants import *
+
+
+class App:
+    def __init__(self):
+        """the costructor - downloads the data into self.prizes (or exits) and enters the application loop"""
+        response = self.get_response(
+            f"http://api.nobelprize.org/2.1/nobelPrizes?sort=asc&nobelPrizeYear={START_YEAR}&yearTo={datetime.datetime.now().strftime('%Y')}&nobelPrizeCategory={CATEGORY}&format={FORMAT}")
+        if response.status_code != STATUS_CODE_CORRECT:
+            exit(1)
+
+        self.prizes = list(response.json().values())[DATA_INDEX]
+        self.run()
+
+    def get_response(self, href):
+        """gets a response using requests.get() and checks the status code"""
+        response = requests.get(href)
+        status_code = response.status_code
+        if status_code != STATUS_CODE_CORRECT:
+            codes = {400: "BadRequest",
+                     422: "UnprocessableEntity", 404: "NotFound"}
+            print(PRINT_RED.format(
+                f"[API error {status_code}: {codes.get(status_code) if codes.get(status_code) else 'UnknownStatusCode'}], data cannot be fetched."))
+            # exit(1)
+        return response
+
+    def controlled_input(self, regex, error_message):
+        """enters a loop untill the user provides an input matching the regular expression (regex)"""
+        n = input()
+        while not re.match(regex, n):
+            if n.lower() == "exit":
+                return n
+            print(PRINT_RED.format(error_message))
+            n = input()
+        return n
+
+    def print_year(self, year):
+        """Prints names of laureates, year of the prize and organizations they belonged to when they won the prize"""
+        prize = list(
+            filter(lambda prize_element: prize_element["awardYear"] == year, self.prizes))
+        if not prize:
+            print(PRINT_RED.format(
+                f"There are no laureates for given year ({year})."))
+            return
+        else:
+            prize = prize[DATA_INDEX]
+
+        for laureate in prize["laureates"]:
+            id = laureate["id"]
+            # the organizations are fetched from the API for each candidate on the go
+            laurate_response = self.get_response(
+                f"http://api.nobelprize.org/2.1/laureate/{id}")
+            organization = "[Could fetch organization]"
+            if laurate_response.status_code == STATUS_CODE_CORRECT:
+                filtered_prize = list(filter(
+                    lambda element: element["awardYear"] == year and element["category"]["en"] == CATEGORY_IN_JSON, laurate_response.json()[DATA_INDEX]["nobelPrizes"]))
+                if filtered_prize:
+                    affiliations = filtered_prize[DATA_INDEX]["affiliations"]
+                    organization = "\nOrganizations:\n"
+                    for affiliation in affiliations:
+                        organization += f"\t- {affiliation['name']['en']}\n"
+
+            print("***\n"
+                  + laureate["fullName"]["en"] + " | "
+                  + year + " | "
+                  + organization)
+
+    def run(self):
+        """the main loop of the application - asks user for a year or to exit"""
+        while True:
+            print("Please enter a year above 2000 (or exit)")
+            user_input = self.controlled_input(
+                "^2[0-9]{3}$", "The input is incorrect: please follow the YYYY year format.")
+            if user_input == "exit":
+                print("Closing the app.")
+                return
+            self.print_year(user_input)
+
+
+if __name__ == "__main__":
+    app = App()
--- README.md
+++ README.md
@@ -1,21 +1,20 @@
-intel recruitment task:
-
-#Task
+# Intel recruitment task
 Write a Python program to query the Nobel Prize API and retrieve information about Nobel Laureates.
 
-#Requirements
-
+# Requirements
 Use the Nobel Prize API to retrieve a list of all Nobel Laureates in Physics who won the prize in the year 2000 or later.
-
 For each laureate, print their full name, year they won the prize, and the university/organization they were affiliated with at the time they won the prize.
-
 Allow the user to enter the year of a prize and print the details for laureates of that specific year.
-
 Handle potential errors from the API gracefully.
-
 Use comments to document your code.
-
 You can start from the documentation at https://www.nobelprize.org/about/developer-zone-2/
 
-#How to run the application
+# How to run the application
+Run App.py with python form command line,
+input years (2000 - current year) to print the prizes
+input 'exit' to exit
 
+# Modules used
+ - datetime - to read the current year for the query
+ - requests - to send GET requests to nobel API
+ - re - to validate user input using regular expressions
